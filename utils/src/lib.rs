fc96c26c4da43fd1f94a81f0fab89ab0bc0f4e59
Anders Lindeberg <ajl@andersjlindeberg.se>
2024-01-18 16:05:55 +0100

    utils

---
+++ utils/src/lib.rs
@@ -0,0 +1,23 @@
+/// See https://stackoverflow.com/questions/28028854/how-do-i-match-enum-values-with-an-integer
+#[macro_export]
+macro_rules! back_to_enum {
+    ($(#[$meta:meta])* $vis:vis enum $name:ident {
+        $($(#[$vmeta:meta])* $vname:ident $(= $val:expr)?,)*
+    }) => {
+        $(#[$meta])*
+        $vis enum $name {
+            $($(#[$vmeta])* $vname $(= $val)?,)*
+        }
+
+        impl std::convert::TryFrom<i32> for $name {
+            type Error = ();
+
+            fn try_from(v: i32) -> Result<Self, Self::Error> {
+                match v {
+                    $(x if x == $name::$vname as i32 => Ok($name::$vname),)*
+                    _ => Err(()),
+                }
+            }
+        }
+    }
+}
